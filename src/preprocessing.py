d32695084b966eaa9598776f7e87298fda6f727a
sidhan goud <24bk1a66h3@stpetershyd.com>
2026-02-04 15:15:02 +0530

    Add text cleaning function with stopwords removal

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,14 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+STOPWORDS = set(stopwords.words('english'))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z ]', '', text)
+    words = text.split()
+    words = [w for w in words if w not in STOPWORDS]
+    return " ".join(words)
 
